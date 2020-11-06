3aa22779ef350ba3c122ca0b2958bbebfdad176c
VanishMax <maxkors2014@gmail.com>
2020-11-06 17:41:01 +0300

    Add FormInput component with usage in Login form

---
+++ src/components/form-input/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styles from './input.module.css';
+import {ComponentProps} from '../../types';
+
+type InputProps = {
+  value?: string,
+  changeHandler?: (val: string) => void,
+  id?: string,
+  placeholder?: string,
+  autocomplete?: 'on'|'off',
+  label?: string,
+  type: 'text'|'email'|'password'|'number',
+  required?: boolean,
+  icon?: JSX.Element,
+  className?: string,
+}
+
+export default function FormInput ({
+  value,
+  changeHandler,
+  id,
+  placeholder,
+  autocomplete,
+  label,
+  type,
+  required,
+  icon,
+  className,
+} : ComponentProps<InputProps>) {
+
+  return (
+    <label className={styles.formInput + (className ? ' ' + className : '')}>
+      {label && (
+        <span>
+          {label}
+          {required && <span>&nbsp;*</span>}
+        </span>
+      )}
+
+      <div>
+        {icon && (
+          <div>{icon}</div>
+        )}
+        <input
+          id={id}
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          required={required}
+          autoComplete={autocomplete}
+          onChange={(e) => changeHandler?.(e.target.value as string)}
+        />
+      </div>
+    </label>
+  );
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../components/card';
+import FormInput from '../components/form-input';
 
 export default function Login () {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+
   return (
     <Card>
       <h2>Log In</h2>
       <form>
+        <FormInput
+          value={email}
+          label="Email"
+          type="email"
+          required
+          changeHandler={(val) => setEmail(val)}
+        />
 
+        <FormInput
+          value={password}
+          className="mt-1"
+          label="Password"
+          type="password"
+          required
+          changeHandler={(val) => setPassword(val)}
+        />
+
+        <FormInput
+          value={passwordConfirm}
+          className="mt-1"
+          label="Confirm password"
+          type="password"
+          required
+          changeHandler={(val) => setPasswordConfirm(val)}
+        />
+
+        <p>
+          {email}
+          {password}
+          {passwordConfirm}
+        </p>
       </form>
     </Card>
   );
